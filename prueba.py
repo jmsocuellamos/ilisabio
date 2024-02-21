6e6cb7e944dba398be4a00a153133149a06465f7
Javier Morales Socuellamos <jm.socuellamos@gmail.com>
2024-02-21 19:49:37 +0100

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -1,24 +1,23 @@
-## Libreria scikit-survival
-#!pip install scikit-survival
-## Librerias de tratamiento de datos y entorno gráfico
-import os
-import numpy as np      # importamos numpy como np
-import pandas as pd     # importamos pandas como pd
-import math             # importamos módulo para cáculos matemáticos
-import random
-
-# Configuración de entorno gráfico
-######################################
-%matplotlib inline
-import matplotlib.pyplot as plt # importamos matplotlib como plt
-import seaborn as sns # importamos seaborn como sns
-sns.set_style("ticks")
-%config InlineBackend.figure_format = 'retina'
-
-# Módulos scikit-survival
+# Carga de datos
 ###########################
-from sksurv.linear_model import CoxPHSurvivalAnalysis, CoxnetSurvivalAnalysis
-from sksurv.ensemble import RandomSurvivalForest
-from sksurv.metrics import (
-    concordance_index_censored,concordance_index_ipcw,cumulative_dynamic_auc
-)
+
+# Inputs originales
+url = 'https://raw.githubusercontent.com/jmsocuellamos/ilisabio/main/inputs_ori.csv'
+X = pd.read_csv(url)
+# Inputs preprocesados
+url = 'https://raw.githubusercontent.com/jmsocuellamos/ilisabio/main/inputs_pp.csv'
+X_pp = pd.read_csv(url)
+# Survival data
+url = 'https://raw.githubusercontent.com/jmsocuellamos/ilisabio/main/survival.csv'
+survival = pd.read_csv(url)
+# Creamos Structured arrays necesario para los modelos de supervivencia
+y = np.zeros(X_pp.shape[0], dtype={'names':('Status', 'Survival'),
+                          'formats':('?', 'f8')})
+y['Status'] = survival['ICCenseguimiento']
+y['Survival'] = survival['meses_hasta_ICC']
+
+print("")
+print("")
+print("\033[1m" + "Datos cargados con éxito" + "\033[0m")
+print("")
+print("")
