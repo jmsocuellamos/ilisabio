5e9886e26b76fadb96cd1914f862ab168a0e33e9
Javier Morales Socuellamos <jm.socuellamos@gmail.com>
2024-02-21 19:47:38 +0100

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -1,5 +1,5 @@
 ## Libreria scikit-survival
-!pip install scikit-survival
+#!pip install scikit-survival
 ## Librerias de tratamiento de datos y entorno gráfico
 import os
 import numpy as np      # importamos numpy como np
