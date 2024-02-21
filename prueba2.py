aed3444adf8e68b8d2283603ffc353b6757b02e0
Javier Morales Socuellamos <jm.socuellamos@gmail.com>
2024-02-21 20:02:15 +0100

    Update prueba2.py

--- prueba2.py
+++ prueba2.py
@@ -1,3 +1,34 @@
+# Carga de datos
+###########################
+
+import os
+import numpy as np      # importamos numpy como np
+import pandas as pd     # importamos pandas como pd
+import math             # importamos módulo para cáculos matemáticos
+import random
+
+# Inputs originales
+url = 'https://raw.githubusercontent.com/jmsocuellamos/ilisabio/main/inputs_ori.csv'
+X = pd.read_csv(url)
+# Inputs preprocesados
+url = 'https://raw.githubusercontent.com/jmsocuellamos/ilisabio/main/inputs_pp.csv'
+X_pp = pd.read_csv(url)
+# Survival data
+url = 'https://raw.githubusercontent.com/jmsocuellamos/ilisabio/main/survival.csv'
+survival = pd.read_csv(url)
+# Creamos Structured arrays necesario para los modelos de supervivencia
+y = np.zeros(X_pp.shape[0], dtype={'names':('Status', 'Survival'),
+                          'formats':('?', 'f8')})
+y['Status'] = survival['ICCenseguimiento']
+y['Survival'] = survival['meses_hasta_ICC']
+
+print("")
+print("")
+print("\033[1m" + "Datos cargados con éxito" + "\033[0m")
+print("")
+print("")
+
+
 # Modelo de RF
 ###########################
 # Selección de variables
